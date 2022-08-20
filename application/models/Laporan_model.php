9e73107f528d36fe114e9736b4bd91344b5d082f
krisnakare <58677889+krisnakare@users.noreply.github.com>
2022-08-20 18:34:15 +0700

    add left join on all get data laporan

--- application/models/Laporan_model.php
+++ application/models/Laporan_model.php
@@ -13,14 +13,14 @@ class Laporan_model extends CI_Model
 
     public function getAllLaporan()
     {
-        $this->db->join('detail_pengembalian', 'detail_pengembalian.invoice_id = tabel_sewa.invoice_id');
+        $this->db->join('detail_pengembalian', 'detail_pengembalian.invoice_id = tabel_sewa.invoice_id', 'left');
         $this->db->join('detail_sewa', 'detail_sewa.invoice_id = tabel_sewa.invoice_id');
         return $this->db->get('tabel_sewa')->result();
     }
 
     public function filterByTime($month, $year)
     {
-        $this->db->join('detail_pengembalian', 'detail_pengembalian.invoice_id = tabel_sewa.invoice_id');
+        $this->db->join('detail_pengembalian', 'detail_pengembalian.invoice_id = tabel_sewa.invoice_id', 'left');
         $this->db->join('detail_sewa', 'detail_sewa.invoice_id = tabel_sewa.invoice_id');
         $this->db->where('month(tabel_sewa.tgl_sewa)', $month);
         $this->db->where('year(tabel_sewa.tgl_sewa)', $year);
