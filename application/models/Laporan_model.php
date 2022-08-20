fba768091d7d014f7881cb1c9ebc1dd2fa279796
krisnakare <58677889+krisnakare@users.noreply.github.com>
2022-08-20 18:21:21 +0700

    add left join in get laporan

--- application/models/Laporan_model.php
+++ application/models/Laporan_model.php
@@ -6,7 +6,7 @@ class Laporan_model extends CI_Model
 {
     public function data($number, $offset)
     {
-        $this->db->join('detail_pengembalian', 'detail_pengembalian.invoice_id = tabel_sewa.invoice_id');
+        $this->db->join('detail_pengembalian', 'detail_pengembalian.invoice_id = tabel_sewa.invoice_id', 'left');
         $this->db->join('detail_sewa', 'detail_sewa.invoice_id = tabel_sewa.invoice_id');
         return $this->db->get('tabel_sewa', $number, $offset)->result();
     }
